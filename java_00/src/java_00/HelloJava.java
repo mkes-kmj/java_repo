96636465165dfcd10518a8ea67576295efd91164
kr620761_amway <Minwoo_Kang@amway.com>
2024-06-19 02:37:12 +0900

    < 2024.06.19 >  - HellJava 업로드

---
+++ java_00/src/java_00/HelloJava.java
@@ -0,0 +1,12 @@
+package java_00;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java !!");
+		
+	}
+
+}
